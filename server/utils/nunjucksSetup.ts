52d27aa074b9230adb892634fd89950b56fdb1b7
theDustRoom <3530672+theDustRoom@users.noreply.github.com>
2024-03-21 09:51:38 +0000

    updating project name after deployment from template repository

--- server/utils/nunjucksSetup.ts
+++ server/utils/nunjucksSetup.ts
@@ -12,7 +12,7 @@ export default function nunjucksSetup(app: express.Express, applicationInfo: App
   app.set('view engine', 'njk')
 
   app.locals.asset_path = '/assets/'
-  app.locals.applicationName = 'HMPPS Typescript Template'
+  app.locals.applicationName = 'Hmpps Jobs Board Ui'
   app.locals.environmentName = config.environmentName
   app.locals.environmentNameColour = config.environmentName === 'PRE-PRODUCTION' ? 'govuk-tag--green' : ''
 
