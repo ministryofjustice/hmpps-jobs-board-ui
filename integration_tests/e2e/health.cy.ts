c53e45bde5800f6153ba5656403e3f95ecd180c8
Gary Dutton <gary.dutton@digital.justice.gov.uk>
2024-07-02 16:10:29 +0100

    project_tidy_and_prep Fixed integration tests

--- integration_tests/e2e/health.cy.ts
+++ integration_tests/e2e/health.cy.ts
@@ -3,42 +3,28 @@ context('Healthcheck', () => {
     beforeEach(() => {
       cy.task('reset')
       cy.task('stubAuthPing')
-      cy.task('stubManageUsersPing')
       cy.task('stubTokenVerificationPing')
     })
 
-    it('Health check page is visible and UP', () => {
-      cy.request('/health').its('body.status').should('equal', 'UP')
+    it('Health check page is visible', () => {
+      cy.request('/health').its('body.healthy').should('equal', true)
     })
 
     it('Ping is visible and UP', () => {
       cy.request('/ping').its('body.status').should('equal', 'UP')
     })
-
-    it('Info is visible', () => {
-      cy.request('/info').its('body').should('exist')
-    })
   })
 
   context('Some unhealthy', () => {
-    beforeEach(() => {
+    it('Reports correctly when token verification down', () => {
       cy.task('reset')
       cy.task('stubAuthPing')
-      cy.task('stubManageUsersPing')
       cy.task('stubTokenVerificationPing', 500)
-    })
 
-    it('Reports correctly when token verification down', () => {
       cy.request({ url: '/health', method: 'GET', failOnStatusCode: false }).then(response => {
-        expect(response.body.components.hmppsAuth.status).to.equal('UP')
-        expect(response.body.components.manageUsersApi.status).to.equal('UP')
-        expect(response.body.components.tokenVerification.status).to.equal('DOWN')
-        expect(response.body.components.tokenVerification.details).to.contain({ status: 500, retries: 2 })
+        expect(response.body.checks.hmppsAuth).to.equal('UP')
+        expect(response.body.checks.tokenVerification).to.contain({ status: 500, retries: 2 })
       })
-    })
-
-    it('Health check page is visible and DOWN', () => {
-      cy.request({ url: '/health', method: 'GET', failOnStatusCode: false }).its('body.status').should('equal', 'DOWN')
     })
   })
 })
