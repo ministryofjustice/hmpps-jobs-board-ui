57aae4b5f6a5d57daf7499442feb9765314a466e
Gary Dutton <gary.dutton@digital.justice.gov.uk>
2024-07-17 08:04:39 +0100

    ESWE-956_add_job_role_and_source Added integration tests

---
+++ integration_tests/e2e/createJob.cy.ts
@@ -0,0 +1,58 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import IndexPage from '../pages/index'
+import EmployerUpdatePage from '../pages/employers/employerUpdate'
+import EmployerReviewPage from '../pages/employers/employerReview'
+import JobRoleUpdatePage from '../pages/jobs/jobRoleUpdate'
+
+context('Sign In', () => {
+  beforeEach(() => {
+    cy.task('reset')
+    cy.task('stubSignIn')
+    cy.task('stubManageUser')
+    cy.task('getEmployers', { page: 1 })
+    cy.signIn()
+  })
+
+  it('Validation error display', () => {
+    cy.visit('/jobs/job/new/role')
+
+    const jobRoleUpdatePage = new JobRoleUpdatePage('Job role and source')
+
+    jobRoleUpdatePage.submitButton().click()
+
+    // Page errors
+    jobRoleUpdatePage.employerIdPageErrorMessage().contains("Select employer or select 'Add an employer'")
+    jobRoleUpdatePage.jobTitlePageErrorMessage().contains('Job title must be 3 characters or more')
+    jobRoleUpdatePage.jobSectorPageErrorMessage().contains('Select a job sector')
+    jobRoleUpdatePage.industrySectorPageErrorMessage().contains('Select an NFN industry sector')
+    jobRoleUpdatePage.numberOfVacanciesPageErrorMessage().contains('Enter number of vacancies')
+    jobRoleUpdatePage.jobSourcePageErrorMessage().contains('Select a job source')
+
+    // Field errors
+    jobRoleUpdatePage.employerIdFieldErrorMessage().contains("Select employer or select 'Add an employer'")
+    jobRoleUpdatePage.jobTitleFieldErrorMessage().contains('Job title must be 3 characters or more')
+    jobRoleUpdatePage.jobSectorFieldErrorMessage().contains('Select a job sector')
+    jobRoleUpdatePage.industrySectorFieldErrorMessage().contains('Select an NFN industry sector')
+    jobRoleUpdatePage.numberOfVacanciesFieldErrorMessage().contains('Enter number of vacancies')
+    jobRoleUpdatePage.jobSourceFieldErrorMessage().contains('Select a job source')
+  })
+
+  it('Create job flow', () => {
+    cy.visit('/jobs/job/new/role')
+
+    const jobRoleUpdatePage = new JobRoleUpdatePage('Job role and source')
+    jobRoleUpdatePage.headerCaption().contains('Add a job - step 1 of 5')
+
+    jobRoleUpdatePage.employerIdField().type('ASDA')
+    jobRoleUpdatePage.employerIdFieldOption(0).click()
+    jobRoleUpdatePage.jobTitleField().type('Test job')
+    jobRoleUpdatePage.jobSectorField().select('OFFICE')
+    jobRoleUpdatePage.industrySectorField().select('ADMIN_SUPPORT')
+    jobRoleUpdatePage.numberOfVacanciesField().type('1')
+    jobRoleUpdatePage.jobSourceField().select('NFN')
+    jobRoleUpdatePage.jobSource2Field().select('PEL')
+    jobRoleUpdatePage.charityField().type('Test charity')
+
+    jobRoleUpdatePage.submitButton().click()
+  })
+})
