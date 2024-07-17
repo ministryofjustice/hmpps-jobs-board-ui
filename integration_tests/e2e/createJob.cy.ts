8c73a01e71a4a978ed1d9872e8a3d84059483074
Gary Dutton <gary.dutton@digital.justice.gov.uk>
2024-07-17 15:13:59 +0100

    ESWE-956_add_job_role_and_source Added integration tests

--- integration_tests/e2e/createJob.cy.ts
+++ integration_tests/e2e/createJob.cy.ts
@@ -3,6 +3,7 @@ import IndexPage from '../pages/index'
 import EmployerUpdatePage from '../pages/employers/employerUpdate'
 import EmployerReviewPage from '../pages/employers/employerReview'
 import JobRoleUpdatePage from '../pages/jobs/jobRoleUpdate'
+import JobContractUpdatePage from '../pages/jobs/jobContractUpdate'
 
 context('Sign In', () => {
   beforeEach(() => {
@@ -35,6 +36,33 @@ context('Sign In', () => {
     jobRoleUpdatePage.industrySectorFieldErrorMessage().contains('Select an NFN industry sector')
     jobRoleUpdatePage.numberOfVacanciesFieldErrorMessage().contains('Enter number of vacancies')
     jobRoleUpdatePage.jobSourceFieldErrorMessage().contains('Select a job source')
+
+    // Move to next page
+    jobRoleUpdatePage.employerIdField().type('ASDA')
+    jobRoleUpdatePage.employerIdFieldOption(0).click()
+    jobRoleUpdatePage.jobTitleField().type('Test job')
+    jobRoleUpdatePage.jobSectorField().select('OFFICE')
+    jobRoleUpdatePage.industrySectorField().select('ADMIN_SUPPORT')
+    jobRoleUpdatePage.numberOfVacanciesField().type('1')
+    jobRoleUpdatePage.jobSourceField().select('NFN')
+    jobRoleUpdatePage.jobSource2Field().select('PEL')
+    jobRoleUpdatePage.charityField().type('Test charity')
+
+    jobRoleUpdatePage.submitButton().click()
+
+    const jobContractUpdatePage = new JobContractUpdatePage('Job location and contract')
+
+    jobContractUpdatePage.submitButton().click()
+
+    jobContractUpdatePage.postcodePageErrorMessage().contains('Enter a job location')
+    jobContractUpdatePage.salaryFromPageErrorMessage().contains('Enter minimum salary amount')
+    jobContractUpdatePage.salaryPeriodPageErrorMessage().contains('Select a salary period')
+    jobContractUpdatePage
+      .nationalMinimumWagePageErrorMessage()
+      .contains('Select whether the job pays minimum wage or not')
+    jobContractUpdatePage.workPatternPageErrorMessage().contains('Select a work pattern')
+    jobContractUpdatePage.contractTypePageErrorMessage().contains('Select a contract type')
+    jobContractUpdatePage.hoursPageErrorMessage().contains('Select the hours for this job')
   })
 
   it('Create job flow', () => {
@@ -54,5 +82,17 @@ context('Sign In', () => {
     jobRoleUpdatePage.charityField().type('Test charity')
 
     jobRoleUpdatePage.submitButton().click()
+
+    const jobContractUpdatePage = new JobContractUpdatePage('Job location and contract')
+
+    jobContractUpdatePage.postcodeField().type('NE157LR')
+    jobContractUpdatePage.salaryFromField().type('25000')
+    jobContractUpdatePage.salaryPeriodField().select('PER_YEAR')
+    jobContractUpdatePage.nationalMinimumWageYes().click()
+    jobContractUpdatePage.workPatternField().select('FLEXI_TIME')
+    jobContractUpdatePage.contractTypeField().select('PERMANENT')
+    jobContractUpdatePage.hoursField().select('FULL_TIME')
+
+    jobContractUpdatePage.submitButton().click()
   })
 })
