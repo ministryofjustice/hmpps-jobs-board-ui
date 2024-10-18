8f66cac202406b303d590a4425a9f03d7d191b0c
mons kapinga <mons.kapinga@digital.justice.gov.uk>
2024-10-18 13:39:36 +0100

    ESWE-1103: setup and configure google tag manager

--- server/utils/nunjucksSetup.ts
+++ server/utils/nunjucksSetup.ts
@@ -79,4 +79,5 @@ export default function nunjucksSetup(app: express.Express, path: pathModule.Pla
   njkEnv.addGlobal('phaseName', config.phaseName)
   njkEnv.addGlobal('encryptUrlParameter', encryptUrlParameter)
   njkEnv.addGlobal('decryptUrlParameter', decryptUrlParameter)
+  njkEnv.addGlobal('googleTagManagerContainerId', config.googleAnalytics.containerId)
 }
