1e7ac66e60d626825453e2b803545a944c0964f0
mons kapinga <mons.kapinga@digital.justice.gov.uk>
2025-09-30 15:32:26 +0100

    ESWE-1393: put title and breadcrumb behing feature flag

--- server/routes/jobsv2/index.ts
+++ server/routes/jobsv2/index.ts
@@ -1,7 +1,10 @@
 import { Router } from 'express'
 import type { Services } from '../../services'
 import jobAddEmployerRoutes from './jobAddEmployer'
+import jobRoleUpdateRoutes from './jobRoleUpdate'
 
 export default function attachRoutes(router: Router, services: Services): void {
   jobAddEmployerRoutes(router, services)
+
+  jobRoleUpdateRoutes(router, services)
 }
