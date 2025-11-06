ddf47d198bfe4cb5a246de07f9a9d30130c3d324
Mary Ford <mary.ford@justice.gov.uk>
2025-11-06 15:04:46 +0000

    Add cypress tests

--- integration_tests/e2e/createJob.cy.ts
+++ integration_tests/e2e/createJob.cy.ts
@@ -1,5 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
+import { skipOn } from '@cypress/skip-test'
 import JobRoleUpdatePage from '../pages/jobs/jobRoleUpdate'
+import JobIsNationalUpdatePage from '../pages/jobs/jobIsNationalUpdate'
 import JobContractUpdatePage from '../pages/jobs/jobContractUpdate'
 import JobRequirementsUpdatePage from '../pages/jobs/jobRequirementsUpdate'
 import JobHowToApplyPage from '../pages/jobs/jobContractHowToApply'
@@ -20,6 +22,10 @@ context('Sign In', () => {
 
   it('Validation error display', () => {
     cy.visit('/jobs/job/new/role/add')
+
+    cy.checkFeatureToggle('nationalJobs', isEnabled => {
+      cy.wrap(isEnabled).as('nationalJobsEnabled')
+    })
 
     // Job role and source page
     const jobRoleUpdatePage = new JobRoleUpdatePage('Job role and source')
@@ -54,6 +60,24 @@ context('Sign In', () => {
     jobRoleUpdatePage.charityNameField().type('Test charity')
 
     jobRoleUpdatePage.submitButton().click()
+
+    cy.get('@nationalJobsEnabled').then(isEnabled => {
+      if (isEnabled) {
+        // National jobs page
+        const jobIsNationalUpdatePage = new JobIsNationalUpdatePage('Is this a national job?')
+        jobIsNationalUpdatePage.submitButton().click()
+
+        // Page errors
+        jobIsNationalUpdatePage.isNationalPageErrorMessage().contains('Select if this is a national job')
+
+        // Field errors
+        jobIsNationalUpdatePage.isNationalFieldErrorMessage().contains('Select if this is a national job')
+
+        // Move to next page
+        jobIsNationalUpdatePage.isNationalFieldNo().click()
+        jobIsNationalUpdatePage.submitButton().click()
+      }
+    })
 
     // Job location and contract page
     const jobContractUpdatePage = new JobContractUpdatePage('Job location and contract')
@@ -138,7 +162,12 @@ context('Sign In', () => {
     jobHowToApplyPage.howToApplyFieldErrorMessage().contains('Enter how to apply details')
   })
 
-  it('Create job flow', () => {
+  it('Create job flow - without national jobs', () => {
+    // Skip this test if national jobs is enabled
+    cy.checkFeatureToggle('nationalJobs', isEnabled => {
+      skipOn(isEnabled)
+    })
+
     cy.visit('/jobs/job/new/role/add')
 
     // Job role and source page
@@ -237,7 +266,122 @@ context('Sign In', () => {
     const indexPage = new IndexPage('Add jobs and employers')
   })
 
-  it('Create job - change links flow', () => {
+  it('Create job flow - with national jobs', () => {
+    // Skip this test if national jobs is not enabled
+    cy.checkFeatureToggle('nationalJobs', isEnabled => {
+      skipOn(!isEnabled)
+    })
+
+    cy.visit('/jobs/job/new/role/add')
+
+    // Job role and source page
+    const jobRoleUpdatePage = new JobRoleUpdatePage('Job role and source')
+    jobRoleUpdatePage.headerCaption().contains('Add a job - step 1 of 6')
+
+    jobRoleUpdatePage.employerIdField().type('ASDA')
+    jobRoleUpdatePage.employerIdFieldOption(0).click()
+    jobRoleUpdatePage.jobTitleField().type('Test job')
+    jobRoleUpdatePage.sectorField().select('OFFICE')
+    jobRoleUpdatePage.industrySectorField().select('ADMIN_SUPPORT')
+    jobRoleUpdatePage.numberOfVacanciesField().type('1')
+    jobRoleUpdatePage.sourcePrimaryField().select('NFN')
+    jobRoleUpdatePage.sourceSecondaryField().select('PEL')
+    jobRoleUpdatePage.charityNameField().type('Test charity')
+
+    jobRoleUpdatePage.submitButton().click()
+
+    // National jobs page
+    const jobIsNationalUpdatePage = new JobIsNationalUpdatePage('Is this a national job?')
+    jobIsNationalUpdatePage.headerCaption().contains('Add a job - step 2 of 6')
+
+    jobIsNationalUpdatePage.isNationalFieldYes().click()
+    jobIsNationalUpdatePage.submitButton().click()
+
+    // Job location and contract page
+    const jobContractUpdatePage = new JobContractUpdatePage('Job location and contract')
+    jobContractUpdatePage.headerCaption().contains('Add a job - step 3 of 6')
+
+    jobContractUpdatePage.salaryFromField().type('25000')
+    jobContractUpdatePage.salaryPeriodField().select('PER_YEAR')
+    jobContractUpdatePage.isPayingAtLeastNationalMinimumWageYes().click()
+    jobContractUpdatePage.workPatternField().select('FLEXI_TIME')
+    jobContractUpdatePage.contractTypeField().select('PERMANENT')
+    jobContractUpdatePage.hoursPerWeekField().select('FULL_TIME')
+
+    jobContractUpdatePage.submitButton().click()
+
+    // Requirements and job description page
+    const jobRequirementsUpdatePage = new JobRequirementsUpdatePage('Requirements and job description')
+    jobContractUpdatePage.headerCaption().contains('Add a job - step 4 of 6')
+
+    jobRequirementsUpdatePage.essentialCriteriaField().type('Some text')
+    jobRequirementsUpdatePage.descriptionField().type('Some text')
+    jobRequirementsUpdatePage.offenceExclusionsFieldValue('OTHER').click()
+    jobRequirementsUpdatePage.offenceExclusionsDetailsField().type('Some text')
+
+    jobRequirementsUpdatePage.submitButton().click()
+
+    // How to apply page
+    const jobHowToApplyPage = new JobHowToApplyPage('How to apply')
+    jobHowToApplyPage.headerCaption().contains('Add a job - step 5 of 6')
+
+    jobHowToApplyPage.isRollingOpportunityFieldNo().click()
+    jobHowToApplyPage.closingDateField.day().type('1')
+    jobHowToApplyPage.closingDateField.month().type('1')
+    jobHowToApplyPage.closingDateField.year().type('2026')
+    jobHowToApplyPage.isOnlyForPrisonLeaversFieldYes().click()
+    jobHowToApplyPage.howToApplyField().type('Some text')
+    jobHowToApplyPage.startDateField.day().type('1')
+    jobHowToApplyPage.startDateField.month().type('1')
+    jobHowToApplyPage.startDateField.year().type('2026')
+    jobHowToApplyPage.supportingDocumentationRequiredFieldValue('OTHER').click()
+    jobHowToApplyPage.supportingDocumentationDetailsField().type('Some text')
+
+    jobHowToApplyPage.submitButton().click()
+
+    const jobReviewPage = new JobReviewPage('Check your answers before adding job')
+    jobReviewPage.headerCaption().contains('Add a job - step 6 of 6')
+
+    jobReviewPage.employerId().contains('ASDA')
+    jobReviewPage.jobTitle().contains('Test job')
+    jobReviewPage.sector().contains('Office or desk-based')
+    jobReviewPage.industrySector().contains('Administration and support services')
+    jobReviewPage.numberOfVacancies().contains('1')
+    jobReviewPage.sourcePrimary().contains('NFN')
+    jobReviewPage.sourceSecondary().contains('PEL')
+    jobReviewPage.charityName().contains('Test charity')
+    jobReviewPage.isNational().contains('Yes')
+    jobReviewPage.salaryFrom().contains('£25000.00')
+    jobReviewPage.salaryTo().contains('Not provided')
+    jobReviewPage.salaryPeriod().contains('Per year')
+    jobReviewPage.additionalSalaryInformation().contains('Not provided')
+    jobReviewPage.isPayingAtLeastNationalMinimumWage().contains('Yes')
+    jobReviewPage.workPattern().contains('Flexi-time')
+    jobReviewPage.contractType().contains('Permanent')
+    jobReviewPage.hoursPerWeek().contains('Full-time (more than 30-39 hours)')
+    jobReviewPage.baseLocation().contains('Not provided')
+    jobReviewPage.essentialCriteria().contains('Some text')
+    jobReviewPage.desirableCriteria().contains('Not provided')
+    jobReviewPage.description().contains('Some text')
+    jobReviewPage.offenceExclusions().contains('Other - Some text')
+    jobReviewPage.isRollingOpportunity().contains('No')
+    jobReviewPage.closingDate().contains(' 1 January 2026')
+    jobReviewPage.isOnlyForPrisonLeavers().contains('Yes')
+    jobReviewPage.startDate().contains('1 January 2026')
+    jobReviewPage.howToApply().contains('Some text')
+    jobReviewPage.supportingDocumentationRequired().contains('Other - Some text')
+
+    jobReviewPage.submitButton().click()
+
+    const indexPage = new IndexPage('Add jobs and employers')
+  })
+
+  it('Create job - change links flow - without national jobs', () => {
+    // Skip this test if national jobs is enabled
+    cy.checkFeatureToggle('nationalJobs', isEnabled => {
+      skipOn(isEnabled)
+    })
+
     cy.visit('/jobs/job/new/role/add')
 
     // Job role and source page
@@ -344,6 +488,237 @@ context('Sign In', () => {
     jobRoleUpdatePage.charityNameField().clear().type('Another charity')
     jobRoleUpdatePage.submitButton().click()
     jobReviewPage.charityName().contains('Another charity')
+
+    // Contract page changes
+    jobReviewPage.postCodeLink().click()
+    jobContractUpdatePage.postCodeField().clear().type('NE35 6DR')
+    jobContractUpdatePage.submitButton().click()
+    jobReviewPage.postCode().contains('NE35 6DR')
+
+    jobReviewPage.salaryFromLink().click()
+    jobContractUpdatePage.salaryFromField().clear().type('350.99')
+    jobContractUpdatePage.submitButton().click()
+    jobReviewPage.salaryFrom().contains('350.99')
+
+    jobReviewPage.salaryToLink().click()
+    jobContractUpdatePage.salaryToField().clear().type('450.99')
+    jobContractUpdatePage.submitButton().click()
+    jobReviewPage.salaryTo().contains('450.99')
+
+    jobReviewPage.salaryPeriodLink().click()
+    jobContractUpdatePage.salaryPeriodField().select('PER_DAY')
+    jobContractUpdatePage.submitButton().click()
+    jobReviewPage.salaryPeriod().contains('Per day')
+
+    jobReviewPage.isPayingAtLeastNationalMinimumWageLink().click()
+    jobContractUpdatePage.isPayingAtLeastNationalMinimumWageNo().click()
+    jobContractUpdatePage.submitButton().click()
+    jobReviewPage.isPayingAtLeastNationalMinimumWage().contains('No')
+
+    jobReviewPage.workPatternLink().click()
+    jobContractUpdatePage.workPatternField().select('ANNUALISED_HOURS')
+    jobContractUpdatePage.submitButton().click()
+    jobReviewPage.workPattern().contains('Annualised hours')
+
+    jobReviewPage.contractTypeLink().click()
+    jobContractUpdatePage.contractTypeField().select('TEMPORARY')
+    jobContractUpdatePage.submitButton().click()
+    jobReviewPage.contractType().contains('Temporary')
+
+    jobReviewPage.hoursPerWeekLink().click()
+    jobContractUpdatePage.hoursPerWeekField().select('PART_TIME')
+    jobContractUpdatePage.submitButton().click()
+    jobReviewPage.hoursPerWeek().contains('Part-time (less than 30 hours)')
+
+    jobReviewPage.additionalSalaryInformationLink().click()
+    jobContractUpdatePage.additionalSalaryInformationField().clear().type('Some info')
+    jobContractUpdatePage.submitButton().click()
+    jobReviewPage.additionalSalaryInformation().contains('Some info')
+
+    jobReviewPage.baseLocationLink().click()
+    jobContractUpdatePage.baseLocationField().select('WORKPLACE')
+    jobContractUpdatePage.submitButton().click()
+    jobReviewPage.baseLocation().contains('Workplace')
+
+    // Requirements page changes
+    jobReviewPage.essentialCriteriaLink().click()
+    jobRequirementsUpdatePage.essentialCriteriaField().clear().type('Some essential text')
+    jobRequirementsUpdatePage.submitButton().click()
+    jobReviewPage.essentialCriteria().contains('Some essential text')
+
+    jobReviewPage.desirableCriteriaLink().click()
+    jobRequirementsUpdatePage.desirableCriteriaField().clear().type('Some desirable text')
+    jobRequirementsUpdatePage.submitButton().click()
+    jobReviewPage.desirableCriteria().contains('Some desirable text')
+
+    jobReviewPage.descriptionLink().click()
+    jobRequirementsUpdatePage.descriptionField().clear().type('Some descriptive text')
+    jobRequirementsUpdatePage.submitButton().click()
+    jobReviewPage.description().contains('Some descriptive text')
+
+    jobReviewPage.offenceExclusionsLink().click()
+    jobRequirementsUpdatePage.offenceExclusionsFieldValue('ARSON').click()
+    jobRequirementsUpdatePage.submitButton().click()
+    jobReviewPage.offenceExclusions().contains('Arson')
+
+    // How to apply page changes
+    jobReviewPage.isRollingOpportunityLink().click()
+    jobHowToApplyPage.isRollingOpportunityFieldYes().click()
+    jobHowToApplyPage.submitButton().click()
+    jobReviewPage.isRollingOpportunity().contains('Yes')
+
+    jobReviewPage.startDateLink().click()
+    jobHowToApplyPage.startDateField.day().clear().type('2')
+    jobHowToApplyPage.startDateField.month().clear().type('3')
+    jobHowToApplyPage.startDateField.year().clear().type('2027')
+    jobHowToApplyPage.submitButton().click()
+    jobReviewPage.startDate().contains('2 March 2027')
+
+    jobReviewPage.isOnlyForPrisonLeaversLink().click()
+    jobHowToApplyPage.isOnlyForPrisonLeaversFieldNo().click()
+    jobHowToApplyPage.submitButton().click()
+    jobReviewPage.isOnlyForPrisonLeavers().contains('No')
+
+    jobReviewPage.howToApplyLink().click()
+    jobHowToApplyPage.howToApplyField().clear().type('Some info to apply')
+    jobHowToApplyPage.submitButton().click()
+    jobReviewPage.howToApply().contains('Some info to apply')
+
+    jobReviewPage.supportingDocumentationRequiredLink().click()
+    jobHowToApplyPage.supportingDocumentationRequiredFieldValue('CV').click()
+    jobHowToApplyPage.supportingDocumentationDetailsField().clear().type('Some more text')
+    jobHowToApplyPage.submitButton().click()
+    jobReviewPage.supportingDocumentationRequired().contains('CV')
+    jobReviewPage.supportingDocumentationRequired().contains('Some more text')
+  })
+
+  it('Create job - change links flow - with national jobs', () => {
+    // Skip this test if national jobs is not enabled
+    cy.checkFeatureToggle('nationalJobs', isEnabled => {
+      skipOn(!isEnabled)
+    })
+
+    cy.visit('/jobs/job/new/role/add')
+
+    // Job role and source page
+    const jobRoleUpdatePage = new JobRoleUpdatePage('Job role and source')
+    jobRoleUpdatePage.headerCaption().contains('Add a job - step 1 of 6')
+
+    jobRoleUpdatePage.employerIdField().type('ASDA')
+    jobRoleUpdatePage.employerIdFieldOption(0).click()
+    jobRoleUpdatePage.jobTitleField().type('Test job')
+    jobRoleUpdatePage.sectorField().select('OFFICE')
+    jobRoleUpdatePage.industrySectorField().select('ADMIN_SUPPORT')
+    jobRoleUpdatePage.numberOfVacanciesField().type('1')
+    jobRoleUpdatePage.sourcePrimaryField().select('NFN')
+    jobRoleUpdatePage.sourceSecondaryField().select('PEL')
+    jobRoleUpdatePage.charityNameField().type('Test charity')
+
+    jobRoleUpdatePage.submitButton().click()
+
+    // National jobs page
+    const jobIsNationalUpdatePage = new JobIsNationalUpdatePage('Is this a national job?')
+    jobIsNationalUpdatePage.headerCaption().contains('Add a job - step 2 of 6')
+
+    jobIsNationalUpdatePage.isNationalFieldYes().click()
+    jobIsNationalUpdatePage.submitButton().click()
+
+    // Job location and contract page
+    const jobContractUpdatePage = new JobContractUpdatePage('Job location and contract')
+    jobContractUpdatePage.headerCaption().contains('Add a job - step 3 of 6')
+
+    jobContractUpdatePage.salaryFromField().type('25000')
+    jobContractUpdatePage.salaryPeriodField().select('PER_YEAR')
+    jobContractUpdatePage.isPayingAtLeastNationalMinimumWageYes().click()
+    jobContractUpdatePage.workPatternField().select('FLEXI_TIME')
+    jobContractUpdatePage.contractTypeField().select('PERMANENT')
+    jobContractUpdatePage.hoursPerWeekField().select('FULL_TIME')
+
+    jobContractUpdatePage.submitButton().click()
+
+    // Requirements and job description page
+    const jobRequirementsUpdatePage = new JobRequirementsUpdatePage('Requirements and job description')
+    jobContractUpdatePage.headerCaption().contains('Add a job - step 4 of 6')
+
+    jobRequirementsUpdatePage.essentialCriteriaField().type('Some text')
+    jobRequirementsUpdatePage.descriptionField().type('Some text')
+    jobRequirementsUpdatePage.offenceExclusionsFieldValue('NONE').click()
+
+    jobRequirementsUpdatePage.submitButton().click()
+
+    // How to apply page
+    const jobHowToApplyPage = new JobHowToApplyPage('How to apply')
+    jobHowToApplyPage.headerCaption().contains('Add a job - step 5 of 6')
+
+    jobHowToApplyPage.isRollingOpportunityFieldNo().click()
+    jobHowToApplyPage.closingDateField.day().type('1')
+    jobHowToApplyPage.closingDateField.month().type('1')
+    jobHowToApplyPage.closingDateField.year().type('2026')
+    jobHowToApplyPage.isOnlyForPrisonLeaversFieldYes().click()
+    jobHowToApplyPage.howToApplyField().type('Some text')
+    jobHowToApplyPage.startDateField.day().type('1')
+    jobHowToApplyPage.startDateField.month().type('1')
+    jobHowToApplyPage.startDateField.year().type('2026')
+    jobHowToApplyPage.supportingDocumentationRequiredFieldValue('OTHER').click()
+    jobHowToApplyPage.supportingDocumentationDetailsField().type('Some text')
+
+    jobHowToApplyPage.submitButton().click()
+
+    // Check change links function correctly and update values
+
+    // Role page changes
+    const jobReviewPage = new JobReviewPage('Check your answers before adding job')
+    jobReviewPage.headerCaption().contains('Add a job - step 6 of 6')
+
+    jobReviewPage.employerIdLink().click()
+    jobRoleUpdatePage.employerIdField().clear().type('Tesco')
+    jobRoleUpdatePage.employerIdFieldOption(0).click()
+    jobRoleUpdatePage.submitButton().click()
+    jobReviewPage.employerId().contains('Tesco')
+
+    jobReviewPage.jobTitleLink().click()
+    jobRoleUpdatePage.jobTitleField().clear().type('A different job')
+    jobRoleUpdatePage.submitButton().click()
+    jobReviewPage.jobTitle().contains('A different job')
+
+    jobReviewPage.sectorLink().click()
+    jobRoleUpdatePage.sectorField().select('RETAIL')
+    jobRoleUpdatePage.submitButton().click()
+    jobReviewPage.sector().contains('Retail and sales')
+
+    jobReviewPage.industrySectorLink().click()
+    jobRoleUpdatePage.industrySectorField().select('RETAIL')
+    jobRoleUpdatePage.submitButton().click()
+    jobReviewPage.industrySector().contains('Retail (includes wholesale and motor vehicle repair)')
+
+    jobReviewPage.numberOfVacanciesLink().click()
+    jobRoleUpdatePage.numberOfVacanciesField().clear().type('2')
+    jobRoleUpdatePage.submitButton().click()
+    jobReviewPage.numberOfVacancies().contains('2')
+
+    jobReviewPage.sourcePrimaryLink().click()
+    jobRoleUpdatePage.sourcePrimaryField().select('PRISON')
+    jobRoleUpdatePage.submitButton().click()
+    jobReviewPage.sourcePrimary().contains('Prison')
+
+    jobReviewPage.sourceSecondaryLink().click()
+    jobRoleUpdatePage.sourceSecondaryField().select('NFN')
+    jobRoleUpdatePage.submitButton().click()
+    jobReviewPage.sourceSecondary().contains('NFN')
+
+    jobReviewPage.charityNameLink().click()
+    jobRoleUpdatePage.charityNameField().clear().type('Another charity')
+    jobRoleUpdatePage.submitButton().click()
+    jobReviewPage.charityName().contains('Another charity')
+
+    // National job change
+    jobReviewPage.isNationalLink().click()
+    jobIsNationalUpdatePage.isNationalFieldNo().click()
+    jobIsNationalUpdatePage.submitButton().click()
+    jobContractUpdatePage.postCodeField().clear().type('NE15 7LR')
+    jobContractUpdatePage.submitButton().click()
+    jobReviewPage.isNational().contains('No')
+    jobReviewPage.postCode().contains('NE15 7LR')
 
     // Contract page changes
     jobReviewPage.postCodeLink().click()
