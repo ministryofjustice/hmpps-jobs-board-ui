dcc221b0e0a404696f16667c011da3d18cd9957d
monskapinga <mons.kapinga1@justice.gov.uk>
2026-01-19 11:39:12 +0000

    ESWE-1378: update cypress config

--- integration_tests/index.d.ts
+++ integration_tests/index.d.ts
@@ -5,10 +5,8 @@ declare namespace Cypress {
      * @example cy.signIn({ failOnStatusCode: boolean })
      */
     signIn(options?: { failOnStatusCode: boolean }): Chainable<AUTWindow>
-
     /**
      * Custom command to check if a feature is enabled.
-     * @example cy.checkFeatureToggle('filterJobsCreatedByMeEnabled', (isEnabled) => { ... })
      */
     checkFeatureToggle(featureName: string, callback: (isEnabled: boolean) => void): Chainable<void>
   }
