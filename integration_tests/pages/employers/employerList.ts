27e703353e26274e7ba736e432d61d7b5b536436
mons kapinga <mons.kapinga1@justice.gov.uk>
2026-05-08 16:56:55 +0100

    ESWE-1872: fix cypress tests

--- integration_tests/pages/employers/employerList.ts
+++ integration_tests/pages/employers/employerList.ts
@@ -6,9 +6,9 @@ export default class EmployerListPage extends Page {
   employerLink = (index: number) => cy.get(`#employer-link-${index}`)
 
   // pagination
-  nextLink = () => cy.get('.moj-pagination__item--next')
+  nextLink = () => cy.get('.govuk-pagination__next')
 
-  previousLink = () => cy.get('.moj-pagination__item--prev')
+  previousLink = () => cy.get('.govuk-pagination__prev')
 
   paginationResults = () => cy.get('.moj-pagination__results')
 
