0aa16a618e7d5bc17b999b519db505af4ef71f86
Mary Ford <mary.ford@justice.gov.uk>
2026-09-24 15:04:28 +0100

    Migrate to hmpps-azure-telemetry and add userUUID to logs

--- server/utils/azureAppInsights.ts
+++ server/utils/azureAppInsights.ts
@@ -1,80 +1,21 @@
-import {
-  Contracts,
-  defaultClient,
-  DistributedTracingModes,
-  getCorrelationContext,
-  setup,
-  TelemetryClient,
-} from 'applicationinsights'
-import { RequestHandler } from 'express'
-import { EnvelopeTelemetry } from 'applicationinsights/out/Declarations/Contracts'
-import type { ApplicationInfo } from '../applicationInfo'
+import { initialiseTelemetry, flushTelemetry, telemetry } from '@ministryofjustice/hmpps-azure-telemetry'
+import logger from '../../logger'
 
-export type ContextObject = {
-  /* eslint-disable  @typescript-eslint/no-explicit-any */
-  [name: string]: any
+initialiseTelemetry({
+  serviceName: 'hmpps-jobs-board-ui',
+  serviceVersion: process.env.BUILD_NUMBER || 'unknown',
+  connectionString: process.env.APPLICATIONINSIGHTS_CONNECTION_STRING,
+  debug: process.env.DEBUG_TELEMETRY === 'true',
+})
+  .addFilter(telemetry.processors.filterSpanWherePath(['/health', '/ping', '/info', '/assets/*', '/favicon.ico']))
+  .addModifier(telemetry.processors.enrichSpanNameWithHttpRoute())
+  .startRecording()
+
+const shutdown = async (signal: string) => {
+  logger.info(`${signal} received, shutting down...`)
+  await flushTelemetry()
+  process.exit(0)
 }
 
-export function initialiseAppInsights(): void {
-  if (process.env.APPLICATIONINSIGHTS_CONNECTION_STRING) {
-    // eslint-disable-next-line no-console
-    console.log('Enabling azure application insights')
-
-    setup().setDistributedTracingMode(DistributedTracingModes.AI_AND_W3C).start()
-  }
-}
-
-export function buildAppInsightsClient(
-  { applicationName, buildNumber }: ApplicationInfo,
-  overrideName?: string,
-): TelemetryClient {
-  if (process.env.APPLICATIONINSIGHTS_CONNECTION_STRING) {
-    defaultClient.context.tags['ai.cloud.role'] = overrideName || applicationName
-    defaultClient.context.tags['ai.application.ver'] = buildNumber
-
-    defaultClient.addTelemetryProcessor(addUserDataToRequests)
-
-    defaultClient.addTelemetryProcessor(({ tags, data }, contextObjects) => {
-      const customProperties = contextObjects.correlationContext?.customProperties
-      const operationNameOverride =
-        customProperties instanceof Map ? customProperties.get('operationName') : customProperties?.operationName
-
-      if (operationNameOverride) {
-        tags['ai.operation.name'] = data.baseData.name = operationNameOverride // eslint-disable-line no-param-reassign,no-multi-assign
-      }
-      return true
-    })
-
-    return defaultClient
-  }
-  return null
-}
-
-export function appInsightsMiddleware(): RequestHandler {
-  return (req, res, next) => {
-    res.prependOnceListener('finish', () => {
-      const context = getCorrelationContext()
-      if (context && req.route) {
-        context.customProperties.setProperty('operationName', `${req.method} ${req.route?.path}`)
-      }
-    })
-    next()
-  }
-}
-
-export function addUserDataToRequests(envelope: EnvelopeTelemetry, contextObjects: ContextObject) {
-  const isRequest = envelope.data.baseType === Contracts.TelemetryTypeString.Request
-  if (isRequest) {
-    const { username, activeCaseLoadId, isReceptionUser } =
-      contextObjects?.['http.ServerRequest']?.res?.locals?.user || {}
-    const { properties } = envelope.data.baseData
-    // eslint-disable-next-line no-param-reassign
-    envelope.data.baseData.properties = {
-      username,
-      activeCaseLoadId,
-      isReceptionUser,
-      ...properties,
-    }
-  }
-  return true
-}
+process.on('SIGTERM', () => shutdown('SIGTERM'))
+process.on('SIGINT', () => shutdown('SIGINT'))
